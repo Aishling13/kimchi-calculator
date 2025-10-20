a457c6e3e20bea2f0c85eb4ca62db8ebe6f31966
Chloansible <chloe.lodge@outlook.com>
2025-10-20 11:57:09 +0100

    feat: Update working salt calculation and result display

--- script.js
+++ script.js
@@ -2,26 +2,29 @@
 psudo-code
 - references to the inputs (vegetable weight and salt percentage)
 - place the display in p element
-- look up what DOM methods to select
 
 */
+let vegWeightInput = document.getElementById("veg-weight");
+let saltPercentInput = document.getElementById("salt-percent");
 const button = document.querySelector("button");
-var saltWeight = document.querySelector("p");
+let saltWeight = document.getElementById("salt-weight");
 
 button.addEventListener("click", calculateSaltWeight);
 
 function calculateSaltWeight() {
-    var vegWeight = document.getElementById("veg-weight").value;
-    var saltPercent = document.getElementById("salt-percent").value;
-    //var vegWeight = document.getElementById("veg-weight").value;
-    //var saltPercent = document.getElementById("salt-percent").value;
+    // convert string to number
+    let vegWeight = parseInt(vegWeightInput.value);
+    let saltPercent = parseInt(saltPercentInput.value);
     
-    // salt = vegetable weight × (salt percentage ÷ 100)
-    saltWeight = vegWeight * (saltPercent % 100);
-
-    //return saltWeight;
-    // console.log("clicked")
+    // calculation | salt = vegetable weight × (salt percentage ÷ 100)
+    let saltWeight = parseInt(vegWeight * (saltPercent / 100));
+    
+    // Debug tests
+    console.log("clicked")
     console.log(vegWeight, saltPercent);
+    console.log(typeof vegWeight, typeof saltPercent);
+    console.log(saltWeight);
 
-}
-// console.log(saltPercent);
+    document.getElementById("salt-weight").innerHTML = saltWeight + "g";
+}   
+
